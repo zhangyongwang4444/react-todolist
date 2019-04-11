ebb92615db322801fb26a0140cc805b2661fc0b3
zhangyongwang <20225614@qq.com>
2019-04-11 08:17:07 +0800

    新建 leanCloud.js

--- src/App.js
+++ src/App.js
@@ -7,14 +7,6 @@ import * as localStore from './localStore'
 import 'normalize.css'
 import './reset.css'
 
-var AV = require('leancloud-storage');
-var APP_ID = 'Cq846eABT06Jse8YwqavT0Aj-gzGzoHsz';
-var APP_KEY = 'P7GhyCurG4aX3b3Q5iaISRAg';
-
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
 
 
 
---
+++ src/leanCloud.js
@@ -0,0 +1,10 @@
+var AV = require('leancloud-storage');
+var APP_ID = 'Cq846eABT06Jse8YwqavT0Aj-gzGzoHsz';
+var APP_KEY = 'P7GhyCurG4aX3b3Q5iaISRAg';
+
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+});
+
+export default AV
