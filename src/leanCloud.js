09da4c25f8e8944958ff7d268651176368cbfb66
zhangyongwang <20225614@qq.com>
2019-04-11 20:25:01 +0800

    点击注册后使用 LeanCloud API 注册

--- src/leanCloud.js
+++ src/leanCloud.js
@@ -8,3 +8,34 @@ AV.init({
 });
 
 export default AV
+
+export function signUp(username, password, successFn, errorFn) {
+    // 新建 AVUser 对象实例
+    var user = new AV.User();
+    // 设置用户名
+    user.setUsername(username);
+    // 设置密码
+    user.setPassword(password);
+
+    user.signUp().then(function (loggedInUser) {
+        console.log('loggedInUser')
+        console.log(loggedInUser)
+        let user = getUserFromAVUser(loggedInUser)
+        successFn.call(null, user)
+    }, function (error) {
+        errorFn.call(null, error)
+    });
+
+
+    return undefined
+}
+
+
+function getUserFromAVUser(AVUser) {
+    console.log('AVUser')
+    console.log(AVUser)
+    return {
+        id: AVUser.id,
+        ...AVUser.attributes
+    }
+}
