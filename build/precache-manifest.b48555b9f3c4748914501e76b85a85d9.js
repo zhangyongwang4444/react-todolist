29c450d81809bab3f8341ce688a3b38d22c87f7e
zhangyongwang <20225614@qq.com>
2019-04-14 14:34:01 +0800

    部署到github

--- build/precache-manifest.b48555b9f3c4748914501e76b85a85d9.js
+++ build/precache-manifest.b48555b9f3c4748914501e76b85a85d9.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/react-todolist/build/static/js/runtime~main.035a3c40.js"
   },
   {
-    "revision": "67d3246ef945ab86d554",
-    "url": "/react-todolist/build/static/js/main.0e799c96.chunk.js"
+    "revision": "b95446f62631fe0720e5",
+    "url": "/react-todolist/build/static/js/main.f4b3faaf.chunk.js"
   },
   {
-    "revision": "a5dd13fd6b010bdf0464",
-    "url": "/react-todolist/build/static/js/2.99f24c2d.chunk.js"
+    "revision": "0aa84cc2667e3e029699",
+    "url": "/react-todolist/build/static/js/2.cded74b5.chunk.js"
   },
   {
-    "revision": "67d3246ef945ab86d554",
+    "revision": "b95446f62631fe0720e5",
     "url": "/react-todolist/build/static/css/main.c082df5e.chunk.css"
   },
   {
-    "revision": "a5dd13fd6b010bdf0464",
+    "revision": "0aa84cc2667e3e029699",
     "url": "/react-todolist/build/static/css/2.5cdbdde7.chunk.css"
   },
   {
-    "revision": "6e1021be07c1a032a405f9ddf2d1a841",
+    "revision": "787c1f173d89d6570a27c3c651d31f4c",
     "url": "/react-todolist/build/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-todolist/build/precache-manifest.d7f10422523bc5c6292323cb1c53f92b.js"
+  "/react-todolist/build/precache-manifest.b48555b9f3c4748914501e76b85a85d9.js"
 );
 
 workbox.clientsClaim();
